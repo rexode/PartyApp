58b378113434ef442434709698c0062a15a5cec2
Joelle Wagner <joy.wagner@tum.de>
2021-11-27 00:20:42 +0100

    fixed layouts

--- app/src/main/java/com/example/partyapp/PartyAdding.kt
+++ app/src/main/java/com/example/partyapp/PartyAdding.kt
@@ -3,6 +3,7 @@ package com.example.partyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.enter_name_fragment.*
@@ -19,16 +20,21 @@ class PartyAdding : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.enter_partyinfo_fragment)
+
         //val id =intent.getStringExtra("id")
         val factory = PartyViewModelFactory()
         val intent = Intent(this, AllParties::class.java)
+
         partyViewModel = ViewModelProvider(this).get(PartyViewModel::class.java)
+
         button_done.setOnClickListener{
-            val party=Party(textedit_party_name.text.toString(),textedit_party_time.text.toString(),textedit_party_location.text.toString(),textedit_party_additionalInfo.text.toString())
+            val party=Party(textedit_party_name.text.toString(),textview_party_time.text.toString(),textedit_party_location.text.toString(),textedit_party_additionalInfo.text.toString())
             partyViewModel.addParty(party)
             //intent.putExtra("UserId",)
 
             startActivity(intent)
         }
+
+
     }
 }
--- app/src/main/java/com/example/partyapp/PartyInfoDialogFragment.kt
+++ app/src/main/java/com/example/partyapp/PartyInfoDialogFragment.kt
@@ -1,5 +1,8 @@
 package com.example.partyapp
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.LayoutInflater
@@ -13,10 +16,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.enter_name_fragment.*
 import kotlinx.android.synthetic.main.enter_partyinfo_fragment.button_done
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class PartyInfoDialogFragment: DialogFragment() {
     private lateinit var partyViewModel: PartyViewModel
+
 
 
     override fun onCreateView(inflater: LayoutInflater,
@@ -25,22 +31,57 @@ class PartyInfoDialogFragment: DialogFragment() {
         var rootView: View = inflater.inflate(R.layout.enter_partyinfo_fragment, container, false)
 
 
+        /*
+       // Date-picker
+        val myCalender = Calendar.getInstance()
+        val datePicker = DatePickerDialog
+            .OnDateSetListener { view, year, month, dayOfMonth ->
+                myCalender.set(Calendar.YEAR, year)
+                myCalender.set(Calendar.MONDAY, month)
+                myCalender.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateLable(myCalender)
+            }
+
+        textview_party_date.setOnClickListener {
+            DatePickerDialog(this
+                , datePicker
+                , myCalender.get(Calendar.YEAR)
+                , myCalender.get(Calendar.MONTH)
+                , myCalender.get(Calendar.DAY_OF_MONTH)).show()
+
+        }
+
+
+
+        // Time-picker
+        textview_party_time.setOnClickListener{
+            setTime()
+        }
+
+
+         */
+
         rootView.button_done.setOnClickListener {
 
             //checking for empty field
-
             if (textedit_party_name.text.isNullOrEmpty()) {
                 Toast.makeText(
                     activity,
                     "Please enter a partyname",
                     Toast.LENGTH_SHORT
                 ).show()
-            } else if(textedit_party_time.text.isNullOrEmpty()){
+            } else if(textview_party_time.text.isNullOrEmpty()) {
                 Toast.makeText(
                     activity,
                     "Please enter a timeslot",
                     Toast.LENGTH_SHORT
                 ).show()
+            } else if(textview_party_date.text.isNullOrEmpty()){
+                    Toast.makeText(
+                        activity,
+                        "Please enter a timeslot",
+                        Toast.LENGTH_SHORT
+                    ).show()
             } else if(textedit_party_location.text.isNullOrEmpty())  {
                 Toast.makeText(
                     activity,
@@ -49,7 +90,7 @@ class PartyInfoDialogFragment: DialogFragment() {
                 ).show()
             } else{
                 partyViewModel = ViewModelProvider(this).get(PartyViewModel::class.java)
-                val party=Party(textedit_party_name.text.toString(),textedit_party_time.text.toString(),textedit_party_location.text.toString(),textedit_party_additionalInfo.text.toString())
+                val party=Party(textedit_party_name.text.toString(),textview_party_time.text.toString(),textedit_party_location.text.toString(),textedit_party_additionalInfo.text.toString())
                 partyViewModel.addParty(party)
                 dismiss()
             }
@@ -58,4 +99,29 @@ class PartyInfoDialogFragment: DialogFragment() {
 
         return rootView
     }
+/*
+    // Dateformatter
+    private fun updateLable(myCalender: Calendar) {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.GERMANY)
+        textview_party_date.setText(sdf.format(myCalender.time))
+    }
+
+
+    // Timepicker -logic
+    private fun setTime() {
+
+        val cal = Calendar.getInstance()
+        val timeSetListener = TimePickerDialog
+            .OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                textview_party_time.text = SimpleDateFormat("HH:mm")
+                    .format(cal.time)
+            }
+        TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
+            cal.get(Calendar.MINUTE), true).show()
+    }
+
+ */
 }
