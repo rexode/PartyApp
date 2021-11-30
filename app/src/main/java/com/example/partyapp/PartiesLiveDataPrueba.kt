f2282b0fcfc4e5af34212b0043289176b522da9e
Albion <albion.b@hotmail.de>
2021-11-30 10:21:29 +0100

    pushing santiagos change

--- app/src/main/java/com/example/partyapp/PartiesLiveDataPrueba.kt
+++ app/src/main/java/com/example/partyapp/PartiesLiveDataPrueba.kt
@@ -2,12 +2,8 @@ package com.example.partyapp
 
 import android.content.ContentValues.TAG
 import android.content.Context
-import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
@@ -15,7 +11,6 @@ import com.google.firebase.database.ktx.getValue
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.all_partys_layout.*
 import java.nio.file.Files.find
 
 class PartiesLiveDataPrueba: MutableLiveData<MutableList<Party>>() {
@@ -27,37 +22,6 @@ class PartiesLiveDataPrueba: MutableLiveData<MutableList<Party>>() {
 
 
     }
-
-//    Party("error","error","error","error")
-
-    fun findParty(id:String?,context: LifecycleOwner,fragmentManager: FragmentManager) {
-        db.collection("Parties").addSnapshotListener{
-                snapshot,e->
-            if(e!=null){
-                Log.w(TAG,"Listen faile",e)
-            }
-            if(snapshot!=null){
-                var document = snapshot.documents
-                document.forEach{
-                    val party=it.toObject(Party::class.java)
-                    if(party!=null && party.uid.equals(id)){
-                        var overlay=  PartyInfo()
-                        var args : Bundle = Bundle()
-                        args.putString("name",party.name)
-                        args.putString("time",party.time)
-                        args.putString("date",party.date)
-                        args.putString("location",party.location)
-                        args.putString("additionalInfo",party.AditionalInfo)
-                        overlay.arguments = args
-                        overlay.show(fragmentManager,
-                            "partyOverlay")
-                    }
-                }
-            }
-
-        }
-    }
-
     fun addParty(party:Party) {
         val id=db.collection("participants").document().id
         party.uid=id
@@ -68,12 +32,7 @@ class PartiesLiveDataPrueba: MutableLiveData<MutableList<Party>>() {
 
 
     }
-    fun updateNote(party:Party) {
-        if (party.uid != null) {
-            // Update note under path /notes/$uid
-            reference.child(party.uid!!).updateChildren(party.toMap())
-        }
-    }
+
     fun deleteNote(note: Party) {
         if (note.uid != null) {
             // Delete note under path /notes/$uid
@@ -113,6 +72,24 @@ class PartiesLiveDataPrueba: MutableLiveData<MutableList<Party>>() {
     fun getParty(id:String){
         val party: Party? = value?.find { it.uid == id }
 
+    }
+    fun findParty(id:String?): Party{
+        var resParty= Party(id,"21312","12312","info")
+        value?.forEach{
+            if(it!=null){
+                if(it.uid==id){
+                    resParty.uid=id
+                    resParty.AditionalInfo=it.AditionalInfo
+                    resParty.name=it.name
+                    resParty.location=it.location
+                    resParty.date=it.date
+                    resParty.time=it.time
+                    resParty.participants=it.participants
+                }
+            }
+        }
+        resParty= value?.get(0)!!
+        return resParty
     }
 
 
