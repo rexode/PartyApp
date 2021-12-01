9a2708109644a92719b067aa4e645c1b7b293fad
Joelle Wagner <joy.wagner@tum.de>
2021-12-01 09:06:58 +0100

    many changes

--- app/src/main/java/com/example/partyapp/PartyInfo.kt
+++ app/src/main/java/com/example/partyapp/PartyInfo.kt
@@ -11,6 +11,9 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -26,13 +29,17 @@ import java.util.*
 
 class PartyInfo() : AppCompatActivity() {
     private lateinit var partyViewModel: PartyViewModel
-    private val dummyParty: Party = Party("name","today","now","here","info")
+    private val dummyParty: Party = Party("name", "today", "now", "here", "info")
 
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     var currentLocation: String = ""
 
-
+    var name: String = ""
+    var date: String = ""
+    var time: String = ""
+    var location: String = ""
+    var addInfo: String = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,15 +51,10 @@ class PartyInfo() : AppCompatActivity() {
         actionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        var name: String = ""
-        var date: String= ""
-        var time :String= ""
-        var location : String= ""
-        var addInfo : String= ""
 
-        partyViewModel= ViewModelProvider(this).get(PartyViewModel::class.java)
-        partyViewModel.findParty(intent.getStringExtra("id")).observe(this,{
-            list->
+
+        partyViewModel = ViewModelProvider(this).get(PartyViewModel::class.java)
+        partyViewModel.findParty(intent.getStringExtra("id")).observe(this, { list ->
             textView_party_name.text = list.get(0).name
             textView_party_time.text = list.get(0).time
             textView_insert_addInfo.text = list.get(0).AditionalInfo
@@ -62,8 +64,8 @@ class PartyInfo() : AppCompatActivity() {
             name = list.get(0).name.toString()
             time = list.get(0).time.toString()
             addInfo = list.get(0).AditionalInfo.toString()
-            location= list.get(0).location.toString()
-            date =list.get(0).date.toString()
+            location = list.get(0).location.toString()
+            date = list.get(0).date.toString()
 
         })
         var partyList = mutableListOf(
@@ -73,13 +75,13 @@ class PartyInfo() : AppCompatActivity() {
             Party("dId", "dName", "dtime", "dhere"),
             Party("eId", "eName", "etime", "ehere"),
         )
-        var liveList:List<User>
+        var liveList: List<User>
         partyViewModel.getParticipants(intent.getStringExtra("id")!!).observe(this, { list ->
             liveList = list
             val adapter = ParticipantAdapter(liveList)
             recyclerviewPartyInfo.adapter = adapter
         })
-       //val adapter = ParticipantAdapter(partyList)
+        //val adapter = ParticipantAdapter(partyList)
         recyclerviewPartyInfo.layoutManager = LinearLayoutManager(this)
         //recyclerviewPartyInfo.adapter = adapter
         //recyclerviewPartyInfo.layoutManager = LinearLayoutManager(this)
@@ -89,73 +91,32 @@ class PartyInfo() : AppCompatActivity() {
 
         textView_location.setOnClickListener {
 
-        var location: String = textView_location.text.toString()
+            var location: String = textView_location.text.toString()
 
-                getLocation()
-                DisplayTrack(location);
-            }
+            getLocation()
+            DisplayTrack(location);
+        }
 
 
-        button_join_party.setOnClickListener{
+        button_join_party.setOnClickListener {
             val sp: SharedPreferences = getSharedPreferences("FILE_NAME", MODE_PRIVATE)
             val uidRestored = sp.getString("key", "")
             val sp2: SharedPreferences = getSharedPreferences("FILE_NAME", MODE_PRIVATE)
             val nameRestored = sp2.getString("username", "")
             val sp3: SharedPreferences = getSharedPreferences("FILE_NAME", MODE_PRIVATE)
             val emailRestored = sp3.getString("email", "")
-            Toast.makeText(this,emailRestored,Toast.LENGTH_SHORT).show()
-            partyViewModel.addParticipants(emailRestored!!, uidRestored!!, nameRestored!!, intent.getStringExtra("id")!!)
+            Toast.makeText(this, emailRestored, Toast.LENGTH_SHORT).show()
+            partyViewModel.addParticipants(
+                emailRestored!!,
+                uidRestored!!,
+                nameRestored!!,
+                intent.getStringExtra("id")!!
+            )
             //partyViewModel.addParticipants()
         }
-        button_leave.setOnClickListener{
-            //NOT TESTED YEEEEEEEEEEEEEET
-            val sp: SharedPreferences = getSharedPreferences("FILE_NAME", MODE_PRIVATE)
-            val uidRestored = sp.getString("key", "")
-            //Toast.makeText(this, "jmGgeoscPYgjjqzW49sjLMx7HJv2",Toast.LENGTH_LONG).show()
-            partyViewModel.removeParticipant(uidRestored!!, intent.getStringExtra("id")!!)
-        }
 
-        button_edit_party.setOnClickListener {
-
-            var partyId = intent.getStringExtra("id")
-
-           /* val bundle = Bundle()
-            bundle.putString("edttext", "From Activity")
-            // set Fragmentclass Arguments
-            // set Fragmentclass Arguments
-            val fragobj = PartyInfoDialogFragment2(partyId)
-            fragobj.setArguments(bundle)
-
-            */
-
-
-           /*  val intent2 = Intent(it.context, PartyInfoDialogFragment2::class.java)
-            intent2.putExtra("partyId",partyId )
-            it.context.startActivity(intent2)
-
-            val sp: SharedPreferences = getSharedPreferences("FILE_NAME", MODE_PRIVATE)
-            val edit : SharedPreferences.Editor = sp.edit()
-            edit.putString("partyId", partyId)
-            edit.apply()
-
-            */
-
-            Toast.makeText(this, partyId, Toast.LENGTH_SHORT).show()
-
-
-            var dialog = PartyInfoDialogFragment2(partyId,name,time, date, location, addInfo)
-            dialog.show(supportFragmentManager, "customDialog") }
-
-        button_delete_party.setOnClickListener{
-            partyViewModel.findParty(intent.getStringExtra("id")!!)
-            var db= FirebaseFirestore.getInstance()
-            db.collection("Parties").document(intent.getStringExtra("id")!!).delete()
-        }
 
     }
-
-
-
 
 
     //open map-intent
@@ -209,4 +170,52 @@ class PartyInfo() : AppCompatActivity() {
         return true
     }
 
+
+    // Menu in tool-bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflator: MenuInflater = menuInflater
+        inflator.inflate(R.menu.dot_menu_partylayout, menu)
+        return true
+    }
+
+
+// **********************************LOGOUT AND PROFILE MENU********************************************************************************
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.leave -> {
+
+                //NOT TESTED YEEEEEEEEEEEEEET
+                val sp: SharedPreferences = getSharedPreferences("FILE_NAME", MODE_PRIVATE)
+                val uidRestored = sp.getString("key", "")
+                //Toast.makeText(this, "jmGgeoscPYgjjqzW49sjLMx7HJv2",Toast.LENGTH_LONG).show()
+                partyViewModel.removeParticipant(uidRestored!!, intent.getStringExtra("id")!!)
+            }
+
+
+            R.id.edit -> {
+
+                var partyId = intent.getStringExtra("id")
+
+
+                Toast.makeText(this, partyId, Toast.LENGTH_SHORT).show()
+
+
+                var dialog = PartyInfoDialogFragment2(partyId, name, time, date, location, addInfo)
+                dialog.show(supportFragmentManager, "customDialog")
+
+            }
+
+            R.id.delete -> {
+
+                partyViewModel.findParty(intent.getStringExtra("id")!!)
+                var db = FirebaseFirestore.getInstance()
+                db.collection("Parties").document(intent.getStringExtra("id")!!).delete()
+
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 }
